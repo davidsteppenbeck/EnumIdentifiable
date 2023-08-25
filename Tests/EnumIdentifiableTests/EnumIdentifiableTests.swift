175e1c41366260f32a97036854c593f5f004ce9c
David Steppenbeck <davidsteppenbeck@googlemail.com>
2023-08-25 16:37:43 +0700

    Initial Commit

---
+++ Tests/EnumIdentifiableTests/EnumIdentifiableTests.swift
@@ -0,0 +1,46 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+// Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
+#if canImport(EnumIdentifiableMacros)
+import EnumIdentifiableMacros
+
+let testMacros: [String: Macro.Type] = [
+    "stringify": StringifyMacro.self,
+]
+#endif
+
+final class EnumIdentifiableTests: XCTestCase {
+    func testMacro() throws {
+        #if canImport(EnumIdentifiableMacros)
+        assertMacroExpansion(
+            """
+            #stringify(a + b)
+            """,
+            expandedSource: """
+            (a + b, "a + b")
+            """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+    func testMacroWithStringLiteral() throws {
+        #if canImport(EnumIdentifiableMacros)
+        assertMacroExpansion(
+            #"""
+            #stringify("Hello, \(name)")
+            """#,
+            expandedSource: #"""
+            ("Hello, \(name)", #""Hello, \(name)""#)
+            """#,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+}
